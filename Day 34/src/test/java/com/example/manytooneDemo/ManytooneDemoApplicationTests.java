68c13929153402d0f163057bf1a9d5d5fa1fbd58
Ashwini Tribhuvan <Ashwinitribhuvan02@gmail.com>
2022-05-18 17:04:54 +0530

    initial commit

---
+++ Day 34/src/test/java/com/example/manytooneDemo/ManytooneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manytooneDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytooneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
