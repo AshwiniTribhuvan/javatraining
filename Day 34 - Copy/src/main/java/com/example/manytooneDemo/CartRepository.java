a72539843e6f25aa208d35949a15cd2c9f2ee861
Ashwini Tribhuvan <Ashwinitribhuvan02@gmail.com>
2022-05-18 17:08:49 +0530

    initial commit
    ManyToOne

--- Day 34 - Copy/src/main/java/com/example/manytooneDemo/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.manytooneDemo;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface CartRepository extends JpaRepositoryImplementation<Cart, Integer> {
-
-}
