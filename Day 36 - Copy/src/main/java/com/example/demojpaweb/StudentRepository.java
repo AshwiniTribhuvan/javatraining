4d2c0ecfa536bbcbac1ab8c6c11587e4fe0d4b5c
Ashwini Tribhuvan <Ashwinitribhuvan02@gmail.com>
2022-05-20 15:36:30 +0530

    initial commit
    Model view Controller

--- Day 36 - Copy/src/main/java/com/example/demojpaweb/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demojpaweb;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface StudentRepository extends JpaRepositoryImplementation<Student, Integer> {
-
-}
