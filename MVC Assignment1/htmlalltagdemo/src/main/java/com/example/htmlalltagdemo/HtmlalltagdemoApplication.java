8ab02a50eaacdf2b2b58a4f0c318881077f392d8
Ashwini Tribhuvan <Ashwinitribhuvan02@gmail.com>
2022-05-21 14:47:14 +0530

    initial commit
    The MVC model view controller is a Design pattern in web development field.

---
+++ MVC Assignment1/htmlalltagdemo/src/main/java/com/example/htmlalltagdemo/HtmlalltagdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlalltagdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlalltagdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlalltagdemoApplication.class, args);
+	}
+
+}
