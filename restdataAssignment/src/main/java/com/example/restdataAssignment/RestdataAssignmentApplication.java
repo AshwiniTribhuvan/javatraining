4c8bb55e819c32a480a172813e9022f21efc083a
Ashwini Tribhuvan <Ashwinitribhuvan02@gmail.com>
2022-06-01 12:12:10 +0530

    initial commit

---
+++ restdataAssignment/src/main/java/com/example/restdataAssignment/RestdataAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdataAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdataAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdataAssignmentApplication.class, args);
+	}
+
+}
---
+++ restdataAssignment/src/test/java/com/example/restdataAssignment/RestdataAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdataAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdataAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
